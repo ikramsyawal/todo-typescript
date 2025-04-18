df684e6f453993437655cc48685e38e1e24b4cfe
ikramsyawal <56379280+ikramsyawal@users.noreply.github.com>
2025-04-18 15:36:52 +0800

    feat(todo) : add toggle and delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer, useState, MouseEvent } from "react";
+import { Todo, Todos, Action } from "./types";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function reducer(state: Todos, action: Action) {
+  switch (action.type) {
+    case "ADD":
+      return [...state, newTodo(action.payload.text)];
+    case "TOGGLE":
+      return state.map((todo) =>
+        todo.id === action.payload.id
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      );
+    case "DELETE":
+      return state.filter((todo) => todo.id !== action.payload.id);
+    default:
+      return state;
+  }
 }
 
-export default App
+function newTodo(text: string): Todo {
+  return {
+    id: Date.now(),
+    text: text,
+    completed: false,
+    createdAt: new Date(),
+  };
+}
+
+const initialState: Todos = [];
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [text, setText] = useState("");
+
+  function handleAdd(e: MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    if (!text) return;
+    dispatch({ type: "ADD", payload: { text: text } });
+    setText("");
+  }
+  return (
+    <div className="p-4">
+      <h1>Todo Apps</h1>
+      <form action="">
+        <label htmlFor="">Add Todo</label>
+        <input
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          type="text"
+          className="border-1"
+        />
+        <button onClick={handleAdd} className="bg-blue-500 border-1">
+          Add
+        </button>
+      </form>
+      <p>to do lists</p>
+      <ul>
+        {state.map((todo: Todo) => (
+          <li key={todo.id}>
+            <div className="flex justify-between">
+              <span className={todo.completed ? "line-through" : ""}>
+                {todo.text}
+              </span>
+              <div>
+                <button
+                  onClick={() =>
+                    dispatch({ type: "TOGGLE", payload: { id: todo.id } })
+                  }
+                >
+                  Done
+                </button>
+                <button
+                  onClick={() =>
+                    dispatch({
+                      type: "DELETE",
+                      payload: { id: todo.id },
+                    })
+                  }
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type Todo = {
+  id: number;
+  text: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
+export type Todos = Todo[];
+
+export type Action =
+  | { type: "ADD"; payload: { text: string } }
+  | { type: "DELETE"; payload: { id: number } }
+  | { type: "TOGGLE"; payload: { id: number } };
